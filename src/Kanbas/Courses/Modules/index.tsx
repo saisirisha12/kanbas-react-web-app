d6872fcad270375f127a46e2cd54a838ba9072a7
Sirisha <saisirisha1299@gmail.com>
2024-10-16 22:31:53 -0400

    images to modules

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -100,6 +100,31 @@ export default function Modules() {
             )}
             </li>
           ))}
+
+
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary"> 
+          <BsGripVertical className="me-2 fs-3" />
+          Course Content 
+          <ModuleControlButtons />
+          </div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+            <BsGripVertical className="me-2 fs-3" />
+              LEARNING OBJECTIVES 
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1"> 
+            <BsGripVertical className="me-2 fs-3" />
+            LESSON 1 
+            <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1"> 
+            <BsGripVertical className="me-2 fs-3" />
+            LESSON 2 <LessonControlButtons />
+            </li>
+          </ul>
+        </li>
         </ul>
   
 
@@ -124,29 +149,7 @@ export default function Modules() {
             </li>
           </ul>
         </li>
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> 
-          <BsGripVertical className="me-2 fs-3" />
-          Week 2 
-          <ModuleControlButtons />
-          </div>
-          <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" />
-              LEARNING OBJECTIVES 
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> 
-            <BsGripVertical className="me-2 fs-3" />
-            LESSON 1 
-            <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> 
-            <BsGripVertical className="me-2 fs-3" />
-            LESSON 2 <LessonControlButtons />
-            </li>
-          </ul>
-        </li>
+        
       </ul>  */}
       </div>
   );}
